206e25bf438f7cda90debf70d98ba2e6559ea813
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2024-05-30 23:07:15 +0530

    implement king movements

--- piece.go
+++ piece.go
@@ -22,3 +22,56 @@ func (p *Pawn) Moves() []string {
 	return moves
 }
 
+type King struct {
+	position Position
+}
+
+func NewKing(position string) *King {
+	return &King{position: ParsePosition(position)}
+}
+
+func (k *King) Moves() []string {
+	var moves []string
+	// move one step forward
+	if k.position.Row-1 >= 0 {
+		newPos := Position{Row: k.position.Row - 1, Col: k.position.Col}
+		moves = append(moves, newPos.String())
+	}
+	// move one step backward
+	if k.position.Row+1 <= 7 {
+		newPos := Position{Row: k.position.Row + 1, Col: k.position.Col}
+		moves = append(moves, newPos.String())
+	}
+	// move one step to the left
+	if k.position.Col-1 >= 0 {
+		newPos := Position{Row: k.position.Row, Col: k.position.Col - 1}
+		moves = append(moves, newPos.String())
+	}
+	// move one step to the right
+	if k.position.Col+1 <= 7 {
+		newPos := Position{Row: k.position.Row, Col: k.position.Col + 1}
+		moves = append(moves, newPos.String())
+	}
+	// move one step diagonally forward to the left
+	if k.position.Row+1 <= 7 && k.position.Col-1 >= 0 {
+		newPos := Position{Row: k.position.Row + 1, Col: k.position.Col - 1}
+		moves = append(moves, newPos.String())
+	}
+	// move one step diagonally forward to the right
+	if k.position.Row+1 <= 7 && k.position.Col+1 <= 7 {
+		newPos := Position{Row: k.position.Row + 1, Col: k.position.Col + 1}
+		moves = append(moves, newPos.String())
+	}
+	// move one step diagonally backward to the left
+	if k.position.Row-1 >= 0 && k.position.Col-1 >= 0 {
+		newPos := Position{Row: k.position.Row - 1, Col: k.position.Col - 1}
+		moves = append(moves, newPos.String())
+	}
+	// move one step diagonally backward to the right
+	if k.position.Row-1 >= 0 && k.position.Col+1 <= 7 {
+		newPos := Position{Row: k.position.Row - 1, Col: k.position.Col + 1}
+		moves = append(moves, newPos.String())
+	}
+	return moves
+}
+
--- piece_test.go
+++ piece_test.go
@@ -67,3 +67,64 @@ func TestPawn_Moves(t *testing.T) {
 	}
 }
 
+func TestKing_Moves(t *testing.T) {
+	type fields struct {
+		position Position
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "King D5",
+			fields: fields{
+				position: ParsePosition("D5"),
+			},
+			want: []string{"D6", "D4", "C5", "E5", "C4", "E4", "C6", "E6"},
+		},
+		{
+			name: "King E8",
+			fields: fields{
+				position: ParsePosition("E8"),
+			},
+			want: []string{"E7", "D8", "F8", "D7", "F7"},
+		},
+		{
+			name: "King A1",
+			fields: fields{
+				position: ParsePosition("A1"),
+			},
+			want: []string{"A2", "B1", "B2"},
+		},
+		{
+			name: "King H6",
+			fields: fields{
+				position: ParsePosition("H6"),
+			},
+			want: []string{"H7", "H5", "G6", "G5", "G7"},
+		},
+		{
+			name: "King C3",
+			fields: fields{
+				position: ParsePosition("C3"),
+			},
+			want: []string{"C4", "C2", "B3", "D3", "B2", "D2", "B4", "D4"},
+		},
+		{
+			name: "King F4",
+			fields: fields{
+				position: ParsePosition("F4"),
+			},
+			want: []string{"F5", "F3", "E4", "G4", "E3", "G3", "E5", "G5"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := NewKing(tt.fields.position.String())
+			if got := k.Moves(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Moves() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
