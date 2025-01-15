85384b59392655508960553076f33f8d39e793f8
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-01-15 21:13:11 +0530

    use reference receiver

--- board.go
+++ board.go
@@ -11,11 +11,11 @@ func EmptyBoard() *Board {
 }
 
 // PositionIsEmpty returns true if the position is empty.
-func (b Board) PositionIsEmpty(pos Position) bool {
+func (b *Board) PositionIsEmpty(pos Position) bool {
 	return b.pieces[pos.File][pos.Rank] == nil
 }
 
 // PositionIsValid returns true if the position is valid.
-func (b Board) PositionIsValid(pos Position) bool {
+func (b *Board) PositionIsValid(pos Position) bool {
 	return pos.File >= 0 && pos.File < 8 && pos.Rank >= 0 && pos.Rank < 8
 }
